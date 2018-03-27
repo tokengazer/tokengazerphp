94d196b29af62b6323ac81ee1df72cf47f61a349
932563595 <932563595@qq.com>
2018-03-27 13:07:53 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/coinmarketcap.php

--- coinmarketcap.php
+++ coinmarketcap.php
@@ -34,35 +34,8 @@ echo $p;
 if($p==8){
 return false;
 }
-header("Location http://tokenworm.applinzi.com/coinmarketcap.php?p=".$page);
+//header("Location http://tokenworm.applinzi.com/coinmarketcap.php?p=".$page);
 // 初始化SaeKV对象
 //访问授权应用的数据
 
-/*$url = 'https://coinmarketcap.com/3';
-$content = file_get_contents_https($url);
-$content=getSonString($content,"<tbody>","</tbody>");
-$url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
-foreach($url1 as $k=>$v){
-$contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
-    $githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
-    $i++;
-}
-$url = 'https://coinmarketcap.com/4';
-$content = file_get_contents_https($url);
-$content=getSonString($content,"<tbody>","</tbody>");
-$url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
-foreach($url1 as $k=>$v){
-$contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
-    $githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
-    $i++;
-}
-$url = 'https://coinmarketcap.com/5';
-$content = file_get_contents_https($url);
-$content=getSonString($content,"<tbody>","</tbody>");
-$url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
-foreach($url1 as $k=>$v){
-$contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
-    $githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
-    $i++;
-}*/
 
