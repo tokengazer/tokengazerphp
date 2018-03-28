37d2ee6f591a5162ed93cf99f8c238280e1088a4
932563595 <932563595@qq.com>
2018-03-28 08:30:36 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getcoinkvdb.php

--- getcoinkvdb.php
+++ getcoinkvdb.php
@@ -2,4 +2,12 @@
 $kv = new SaeKV();
 $ret = $kv->init("xowlw2kmk2");
 $ret=$kv->pkrget('', 1700);
-print_r($ret);
+while (true) {
+   var_dump($ret);
+   end($ret);
+   $start_key = key($ret);
+   $i = count($ret);
+   if ($i < 100) break;
+   $ret = $kv->pkrget('', 1700, $start_key);
+    print_r($ret);
+}
