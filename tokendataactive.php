6ed473fcd8d1e3f68f69e3455924b9707bf085db
932563595 <932563595@qq.com>
2018-03-29 03:31:15 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tokendataactive.php

--- tokendataactive.php
+++ tokendataactive.php
@@ -1,5 +1,5 @@
 <?php
-echo $url='https://www.tokendata.io/icos/active?_='.time().'000';
+$url='https://www.tokendata.io/icos/active?_='.time().'000';
     $data=file_get_contents($url);
 print_r($data);
 ?>
