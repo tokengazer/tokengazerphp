340095b1740c0d3b74b00ea713935287ac3b4843
932563595 <932563595@qq.com>
2018-03-29 04:13:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icoalert2.php

---
+++ icoalert2.php
@@ -0,0 +1,21 @@
+<?php
+function curls($url, $data_string) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'X-AjaxPro-Method:ShowList',
+        'User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36',
+    ));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+                }
+$get_url="https://jbmtbl811x-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.21.1%3Binstantsearch.js%201.11.7%3BJS%20Helper%202.19.0&x-algolia-application-id=JBMTBL811X&x-algolia-api-key=bab8508db4e902d54d1603e9d897a285";
+$json='{"requests":[{"indexName":"icoalert-production-recent-presale","params":"query=&hitsPerPage=50&page=0&filters=preSaleEndDate%20%3E%200%20AND%20preSaleEndDate%20%3C%201522294754&facets=%5B%22preSale%22%5D&tagFilters=&facetFilters=%5B%22preSale%3Atrue%22%5D"}]}';
+$post_datas = curls($get_url, $json);
+echo $post_datas;
+?>
+
