ca2defe1ad9ef2a6a3b2794b79a38690416aaa5a
932563595 <932563595@qq.com>
2018-03-29 02:25:42 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tokendataupcoming.php

--- tokendataupcoming.php
+++ tokendataupcoming.php
@@ -1,5 +1,5 @@
 <?php
-$url='https://www.tokendata.io/icos/upcoming?_='.time().'000';die;
+$url='https://www.tokendata.io/icos/upcoming?_='.time().'000';
     $data=file_get_contents_https($url);
-echo print_r($data);die;
+print_r($data);die;
 ?>
