b8657a83ba66fc8ede4f42c08f0d34b810ded8fc
932563595 <932563595@qq.com>
2018-04-01 13:25:30 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icoalert2.php

--- icoalert2.php
+++ icoalert2.php
@@ -12,6 +12,9 @@ function curls($url, $data_string) {
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
     $data = curl_exec($ch);
     curl_close($ch);
+    if (substr($data, 0,3) == pack("CCC",0xef,0xbb,0xbf)) {
+        $data = substr($data, 3);
+    }
     return $data;
                 }
 $get_url="https://jbmtbl811x-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.21.1%3Binstantsearch.js%201.11.7%3BJS%20Helper%202.19.0&x-algolia-application-id=JBMTBL811X&x-algolia-api-key=bab8508db4e902d54d1603e9d897a285";
