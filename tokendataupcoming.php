f9aa7519532595af7f2508dd8c9140208653aae5
932563595 <932563595@qq.com>
2018-04-01 13:04:40 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tokendataupcoming.php

--- tokendataupcoming.php
+++ tokendataupcoming.php
@@ -1,5 +1,15 @@
 <?php
 $url='https://www.tokendata.io/icos/upcoming?_='.time().'000';
     $data=file_get_contents($url);
-print_r($data);
+$data=json_decode($data,true)['data'];;
+
+foreach($data as $k=>$v){
+$sql="INSERT INTO  `app_tokenworm`.`tokendada` (`id` ,`description` ,`end_date` ,`featured_until` ,`month` ,`name` ,";
+$sql.="`name_lower` ,`start_date` ,`status` ,`website` ,`whitepaper` ,`_id`)";
+$sql.="VALUES (NULL ,  '".$data[$k]['description']."',  '".$data[$k]['end_date']."',  '".$data[$k]['featured_until']."',";  
+$sql.="'".$data[$k]['month']."',  '".$data[$k]['name']."',  ";
+$sql.="'".$data[$k]['name_lower']."',  '".$data[$k]['start_date']."',  '".$data[$k]['status']."',  '".$data[$k]['website']."',  '".$data[$k]['whitepaper']."',  '".$data[$k]['_id']."'";
+$sql.=");";
+    MySQLRunSQL($sql);
+    
 ?>
