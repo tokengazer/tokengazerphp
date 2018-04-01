8fe7ce497a2d86a778e24b327efad1658af29ced
932563595 <932563595@qq.com>
2018-04-01 11:28:03 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icoalert.php

--- icoalert.php
+++ icoalert.php
@@ -16,5 +16,10 @@ function curls($url, $data_string) {
 $get_url="http://jbmtbl811x-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.21.1%3Binstantsearch.js%201.11.7%3BJS%20Helper%202.19.0&x-algolia-application-id=JBMTBL811X&x-algolia-api-key=bab8508db4e902d54d1603e9d897a285";
 $json='{"requests":[{"indexName":"icoalert-production","params":"query=&hitsPerPage=20000&page=0&filters=startDate%20%3E%201522294754%20OR%20startDate%20%3D%20-1%20OR%20endDate%20%3D%20-1&facets=%5B%5D&tagFilters="}]}';
 $post_datas = curls($get_url, $json);
-echo $post_datas;
+
+$datas=json_decode($post_datas)['results'][0]['hits'];
+foreach($datas as $k=>$v){
+$sql="INSERT INTO `app_tokenworm`.`IcoalertData` (`craftEntryId`, `dateCreated`, `description`, `endDate`, `featuredListing`, `icoID`, `kyc`, `objectID`, `preFeaturedListing`, `preSale`, `preSaleEndDate`, `preSaleStartDate`, `preSaleStartDay`, `preSaleStartMonth`, `preSaleStartQuarter`, `preSaleStartYear`, `preSaleTbd`, `reportAvailable`, `reportLink`, `startDate`, `startDay`, `startMonth`, `startQuarter`, `startYear`, `tags`, `tbd`, `title`, `usExcludedOption`, `verified`, `website`) VALUES ('".$v['craftEntryId']."', '".$v['dateCreated']."', '".$v['description']."', '".$v['endDate']."', '".$v['featuredListing']."', '".$v['icoID'].."', '".$v['kyc']."', '".$v['objectID']."', '".$v['preFeaturedListing']."', '".$v['preSale']."', '".$v['preSaleEndDate']."', '".$v['preSaleStartDate']."', '".$v['preSaleStartDay']."', '".$v['preSaleStartMonth']."', '".$v['preSaleStartQuarter']."', '".$v['preSaleStartYear']."', '".$v['preSaleTbd']."', '".$v['reportAvailable']."', '".$v['reportLink']."', '".$v['startDate']."', '".$v['startDay']."', '".$v['startMonth']."', '".$v['startQuarter']."', '".$v['startYear']."', '".$v['tags']."', '".$v['tbd']."', '".$v['title']."', '".$v['usExcludedOption']."', '".$v['verified']."', '".$v['website']."');"
+ MySQLRunSQL($sql);
+}
 ?>
