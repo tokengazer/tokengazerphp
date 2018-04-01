21589ea32bc7c8e3d9729497f46b47a25e53753e
932563595 <932563595@qq.com>
2018-04-01 12:55:31 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tokendataactive.php

--- tokendataactive.php
+++ tokendataactive.php
@@ -3,25 +3,12 @@ $url='https://www.tokendata.io/icos/active?_='.time().'000';
     $data=file_get_contents($url);
 $data=json_decode($data,true);
 foreach($data as $k=>v){
-$sql="INSERT INTO  `app_tokenworm`.`tokendada` (
-`id` ,
-`description` ,
-`end_date` ,
-`featured_until` ,
-`month` ,
-`name` ,
-`name_lower` ,
-`start_date` ,
-`status` ,
-`website` ,
-`whitepaper` ,
-`_id`
-)
-VALUES (
-NULL ,  '".$data[$k]['description']."',  '".$data[$k]['end_date']."',  '".$data[$k]['featured_until']."',  
-'".$data[$k]['month']."',  '".$data[$k]['name']."',  
-'".$data[$k]['name_lower']."',  '".$data[$k]['start_date']."',  '".$data[$k]['status']."',  '".$data[$k]['website']."',  '".$data[$k]['whitepaper']."',  '".$data[$k]."'
-);";
+$sql="INSERT INTO  `app_tokenworm`.`tokendada` (`id` ,`description` ,`end_date` ,`featured_until` ,`month` ,`name` ,";
+$sql.="`name_lower` ,`start_date` ,`status` ,`website` ,`whitepaper` ,`_id`)";
+$sql.="VALUES (NULL ,  '".$data[$k]['description']."',  '".$data[$k]['end_date']."',  '".$data[$k]['featured_until']."',";  
+$sql.="'".$data[$k]['month']."',  '".$data[$k]['name']."',  ";
+$sql.="'".$data[$k]['name_lower']."',  '".$data[$k]['start_date']."',  '".$data[$k]['status']."',  '".$data[$k]['website']."',  '".$data[$k]['whitepaper']."',  '".$data[$k]."'";
+$sql.=");";
     MySQLRunSQL($sql);
 }
 ?>
