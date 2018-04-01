9cc010463334e826d0af8098c00808868e8f4280
932563595 <932563595@qq.com>
2018-04-01 12:56:02 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tokendataactive.php

--- tokendataactive.php
+++ tokendataactive.php
@@ -2,7 +2,7 @@
 $url='https://www.tokendata.io/icos/active?_='.time().'000';
     $data=file_get_contents($url);
 $data=json_decode($data,true);
-foreach($data as $k=>v){
+foreach($data as $k=>$v){
 $sql="INSERT INTO  `app_tokenworm`.`tokendada` (`id` ,`description` ,`end_date` ,`featured_until` ,`month` ,`name` ,";
 $sql.="`name_lower` ,`start_date` ,`status` ,`website` ,`whitepaper` ,`_id`)";
 $sql.="VALUES (NULL ,  '".$data[$k]['description']."',  '".$data[$k]['end_date']."',  '".$data[$k]['featured_until']."',";  
