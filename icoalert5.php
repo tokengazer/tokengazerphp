d6853f5690b8e3632a0c370e81b7c8785a4f4765
932563595 <932563595@qq.com>
2018-04-02 06:52:50 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icoalert5.php

--- icoalert5.php
+++ icoalert5.php
@@ -3,18 +3,17 @@ include('bootstraps.php');
 function curls($url, $data_string) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);//获取头部
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    /*curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'X-AjaxPro-Method:ShowList',
         'User-Agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36',
-    ));
+    ));*/
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
     $data = curl_exec($ch);
     curl_close($ch);
-    if (substr($data, 0,3) == pack("CCC",0xef,0xbb,0xbf)) {
-        $data = substr($data, 3);
-    }
+    
     return $data;
                 }
 $get_url='https://jbmtbl811x-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.21.1%3Binstantsearch.js%201.11.7%3BJS%20Helper%202.19.0&x-algolia-application-id=JBMTBL811X&x-algolia-api-key=bab8508db4e902d54d1603e9d897a285';
