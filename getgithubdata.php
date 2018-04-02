13652abdf79bcf3314c3513de261aeb99eace0b5
932563595 <932563595@qq.com>
2018-04-02 01:07:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getgithubdata.php

--- getgithubdata.php
+++ getgithubdata.php
@@ -2,4 +2,29 @@
 include('bootstraps.php');
 $sql="select githuburl from project_list where githuburl <> ''";
 $list=MySQLGetData($sql);
+foreach($list as $k=>$v){
+$list[$k]['githuburl']=str_replace(",","",$list[$k]['githuburl']);
+    $baseurl=str_replace("github.com","api.gethub.com/repo",$list[$k]['githuburl']);
+    print_r(curls($baseurl));
+}
+function curls($url){
+    $headers = array(
+        'Authorization:token  b26b6fe9c7beaba6edf83661c666d3ad5588b35a',
+        'Accept:application/vnd.github.giant-sentry-fist-preview+json',
+    );
+    $curl = curl_init();
+    //设置抓取的url
+    curl_setopt($curl, CURLOPT_URL, $url);
+    //设置头文件的信息作为数据流输出
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    //设置获取的信息以文件流的形式返回，而不是直接输出。
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0);
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    //执行命令
+    $data = curl_exec($curl);
+    //关闭URL请求
+    curl_close($curl);
+    //显示获得的数据
+  }
 print_r($list);
