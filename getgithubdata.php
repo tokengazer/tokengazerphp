e372aa3991b4411074a4efaee3aaa02f5e77312e
932563595 <932563595@qq.com>
2018-04-02 01:13:50 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getgithubdata.php

--- getgithubdata.php
+++ getgithubdata.php
@@ -16,6 +16,7 @@ function curls($url){
     $curl = curl_init();
     //设置抓取的url
     curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, 1);
     //设置头文件的信息作为数据流输出
     curl_setopt($curl, CURLOPT_HEADER, 0);
     //设置获取的信息以文件流的形式返回，而不是直接输出。
