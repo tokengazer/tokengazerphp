0f3e7ef56e3c4141e7fd8bb71adeb27407dc30be
932563595 <932563595@qq.com>
2018-04-02 01:14:25 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getgithubdata.php

--- getgithubdata.php
+++ getgithubdata.php
@@ -1,6 +1,6 @@
 <?php
 include('bootstraps.php');
-$sql="select githuburl from project_list where githuburl <> '' limit 0,30";
+$sql="select githuburl from project_list where githuburl <> '' limit 0,10";
 $list=MySQLGetData($sql);
 foreach($list as $k=>$v){
 $list[$k]['githuburl']=str_replace(",","",$list[$k]['githuburl']);
