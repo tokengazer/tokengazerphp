7f905a9e184b2b3aefa8989435aa9e892d2d8f88
932563595 <932563595@qq.com>
2018-04-08 01:57:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticoratingdetail.php

--- geticoratingdetail.php
+++ geticoratingdetail.php
@@ -2,8 +2,8 @@
 include('bootstraps.php');
 $url="https://icorating.com/ico/crowd-machine/details/";
 $html=file_get_contents_https($url);
-echo $str =explode("uk-table",$html)[0];
-echo $str=explode("Pre-ICO start date:",$str)[1];
+$str =explode("uk-table",$html)[1];
+$str=explode("Pre-ICO start date:",$str)[1];
 $str=getSonString($str,"<td>","</td>");
 echo $str;
 ?>
