7695b9ab5c13f00258ae436a6c225cc01264e499
932563595 <932563595@qq.com>
2018-04-08 05:48:13 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getetherscandraw.php

--- getetherscandraw.php
+++ getetherscandraw.php
@@ -3,6 +3,9 @@ include('bootstraps.php');
 $url="https://etherscan.io/token/tokenholderchart/0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0?range=100";
     $html=file_get_contents_https($url);
 $json=str_replace("'","\"",str_replace("});","",getSonString($html,"series: ","</script>")));
+$json=str_replace("name","\"name\"",$json);
+$json=str_replace("data","\"data\"",$str);
+$json=str_replace(",]}]","]}]",$json);
 $jsonarr=json_decode($json,true);
 print_r($json);
 ?>
