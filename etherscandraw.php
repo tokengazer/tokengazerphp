54331f7612ff4d5af9f4e406b6a5fc8ee3b61dc5
932563595 <932563595@qq.com>
2018-04-08 06:55:30 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/etherscandraw.php

--- etherscandraw.php
+++ etherscandraw.php
@@ -1,8 +1,7 @@
 <?php
 include('bootstraps.php');
 $sql="select sum(money) as sum,* from etherscan_draw where pid=1";
-
-    $data=MySQLGetData($sql);
+$data=MySQLGetData($sql);
 print_r($data);
-}
+
 ?>
