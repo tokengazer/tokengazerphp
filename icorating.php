5f8ed7e729cecbb3f94a9b641924de56636e743a
932563595 <932563595@qq.com>
2018-04-09 02:29:19 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icorating.php

--- icorating.php
+++ icorating.php
@@ -36,6 +36,12 @@ foreach($str2 as $k=>$v){
         $str3=explode("github.com/",$con1)[1];
         $str4=explode("\"",$str3)[0];
         $arr[$i]['githuburl']="https://github.com/".$str4;
+         $ret = $kv->delete('products:'.$i);
+        $kv->add('products:'.$i, json_encode($arr[$i],true));
+    $sql='insert into project_list (name,logo,githuburl,price,DataSource) values("'.$arr[$i]['name'].'","'.trim($logo).'","'.$arr[$i]['githuburl'].'",0,"icorating");';
+    MySQLRunSQL($sql);
+     $kv->get('products:'.$i);
+    $i++;
        
      } 
     else{
@@ -45,11 +51,6 @@ foreach($str2 as $k=>$v){
         $arr[$i]['logo']=$logo;
     
     }
-    $ret = $kv->delete('products:'.$i);
-        $kv->add('products:'.$i, json_encode($arr[$i],true));
-    $sql='insert into project_list (name,logo,githuburl,price,DataSource) values("'.$arr[$i]['name'].'","'.trim($logo).'","'.$arr[$i]['githuburl'].'",0,"icorating");';
-    MySQLRunSQL($sql);
-     $kv->get('products:'.$i);
-    $i++;
+   
 }
 echo 1;
