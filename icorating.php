fca2e3b975e1c9ca6bcc8dc3c771aea2db302524
932563595 <932563595@qq.com>
2018-04-09 02:25:03 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icorating.php

--- icorating.php
+++ icorating.php
@@ -20,7 +20,6 @@ $end2 = '</tbody>';
 $str3 = getSonString($content, $head2, $end2);
 $str3 = getSonString($str3, '<tbody>', '</tbody>');
 array_push($str2,$str3);
-print_r($str2);die;
 $i=0;
 $arr=array();
 foreach($str2 as $k=>$v){
@@ -32,17 +31,25 @@ foreach($str2 as $k=>$v){
         }else{
         $arr[$i]['name']=$name;
         }
-        $url=getSonString($str2[$k],"<tr data-href='","'>",$str2[$k]);
+         $url=getSonString($str2[$k],"<tr data-href='","'>",$str2[$k]);
         //$con1=file_get_contents_https($url);
         $str3=explode("github.com/",$con1)[1];
         $str4=explode("\"",$str3)[0];
         $arr[$i]['githuburl']="https://github.com/".$str4;
-        $ret = $kv->delete('products:'.$i);
+       
+     } 
+    else{
+        $logo=str_replace("<img src=\"","https://icorating.com",$str2[$k]);
+        $logo=str_replace("\" />","",$logo);
+        $logo=str_replace("</td>","",$logo);
+        $data[$i]['logo']=$logo;
+    
+    }
+    $ret = $kv->delete('products:'.$i);
         $kv->add('products:'.$i, json_encode($arr[$i],true));
-    $sql='insert into project_list (name,githuburl,price,DataSource) values("'.$arr[$i]['name'].'","'.$arr[$i]['githuburl'].'",0,"icorating");';
+    $sql='insert into project_list (name,logo,githuburl,price,DataSource) values("'.$arr[$i]['name'].'","'.$logo.'","'.$arr[$i]['githuburl'].'",0,"icorating");';
     MySQLRunSQL($sql);
      $kv->get('products:'.$i);
-    
-    } 
+    $i++;
 }
 echo 1;
