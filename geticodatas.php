262faf4243032d9b2f6effdff223cb58b18caf31
932563595 <932563595@qq.com>
2018-04-10 07:31:15 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticodatas.php

--- geticodatas.php
+++ geticodatas.php
@@ -11,13 +11,11 @@ $url="https://api.coinmarketcap.com/v1/ticker/".$data[$k]['name']."/";
     $Current_market_value=$results[0]['market_cap_usd'];
     $Current_Circulation=$results[0]['available_supply'];
     $Current_Single_price=$results[0]['price_usd'];
-    echo "https://coinmarketcap.com/currencies/".$name."/";
     $html=file_get_contents_https("https://coinmarketcap.com/currencies/".$data[$k]['name']."/");
-    print_r($html);
     $tmpstr3=explode("<li><span class=\"glyphicon glyphicon-hdd text-gray\" title=\"Source Code\"></span> ",$html)[1];
     $tmpstr2=explode("<a href=\"",$tmpstr3)[1];
     $tmpstr4=explode("\"",$tmpstr2)[0];
-    $githuburl=$tmpstr4;
+    echo $githuburl=$tmpstr4;
     //echo $sql="update ico_Analysis set Current_market_value='".$Current_market_value."',Current_Circulation='".$Current_Circulation."',Current_Single_price='".$Current_Single_price."',Github_url='".$githuburl."' where name='".$data[$k]['name']."'";
     //MySQLRunSQL($sql);
 }
