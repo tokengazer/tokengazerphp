1e75734f2daeddc1b9e4d55b4f199e02142c3d96
932563595 <932563595@qq.com>
2018-04-10 07:40:43 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticodatas.php

--- geticodatas.php
+++ geticodatas.php
@@ -1,6 +1,6 @@
 <?
 include('bootstraps.php');
-echo $sql="select name from ico_Analysis where githuburl=''";
+echo $sql="select name from ico_Analysis where Githuburl=''";
 $data=MySQLGetData($sql);
 foreach($data as $k=>$v){
 /*$url="https://api.coinmarketcap.com/v1/ticker/".$data[$k]['name']."/";
