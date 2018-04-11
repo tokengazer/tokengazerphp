e4711f2e4f6be1dd5bf096d2bab7b347878007f4
932563595 <932563595@qq.com>
2018-04-11 01:29:05 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getgithubdata1.php

--- getgithubdata1.php
+++ getgithubdata1.php
@@ -4,7 +4,7 @@ $sql="select Github_url from ico_Analysis where Github_url <> '' ";
 $list=MySQLGetData($sql);
 foreach($list as $k=>$v){
 //$list[$k]['Github_url']=str_replace(",","",$list[$k]['Github_url']);
-    echo $baseurl=str_replace("https://github.com/","",$list[$k]['Github_url']);
+    $baseurl=str_replace("https://github.com/","",$list[$k]['Github_url']);
     echo $baseurl="https://api.github.com/users/".explode("/",$baseurl)[0];
    // print_r(curls($baseurl));
     /*if(strrpos($baseurl,"/")==strlen($baseurl)-1){
