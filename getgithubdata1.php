567289a62debbabc71f7ac2d500ca9c4fccf7b00
932563595 <932563595@qq.com>
2018-04-11 02:07:37 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getgithubdata1.php

--- getgithubdata1.php
+++ getgithubdata1.php
@@ -16,7 +16,7 @@ foreach($list as $k=>$v){
     $watchers+=$results[$kk]['watchers'];
        $lastupdatetime=bijiaotimes($lastupdatetime,$results[$kk]['pushed_at']);
     }
-    echo $sql="update ico_Analysis set GithubForks=".$forks.",GithubStars=".$stars.",GithubWatches=".$$watcher.",Github_lastupdatetime='".$lastupdatetime."' where id=".$list[$k]['id'];
+    echo $sql="update ico_Analysis set GithubForks=".$forks.",GithubStars=".$stars.",GithubWatches=".$watchers.",Github_lastupdatetime='".$lastupdatetime."' where id=".$list[$k]['id'];
     /*if($k==0){
     break;
     }*/
