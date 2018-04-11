611746a37c9f39ce8cf9bc28087b41b6c6196603
932563595 <932563595@qq.com>
2018-04-11 06:19:09 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icoalert3.php

--- icoalert3.php
+++ icoalert3.php
@@ -18,7 +18,7 @@ function curls($url, $data_string) {
     return $data;
                 }
 $get_url='https://jbmtbl811x-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.21.1%3Binstantsearch.js%201.11.7%3BJS%20Helper%202.19.0&x-algolia-application-id=JBMTBL811X&x-algolia-api-key=bab8508db4e902d54d1603e9d897a285';
-$json='{"requests":[{"indexName":"icoalert-production-presale","params":"query=&hitsPerPage=10000&page=0&filters=preSaleStartDate%20%3E%201522294754%20OR%20preSaleStartDate%20%3D%20-1%20OR%20preSaleEndDate%20%3D%20-1&facets=%5B%22preSale%22%5D&tagFilters=&facetFilters=%5B%22preSale%3Atrue%22%5D"}]}';
+$json='{"requests":[{"indexName":"icoalert-production-presale","params":"query=&hitsPerPage=10000&page=0&filters=preSaleStartDate%20%3E%20'.time().'%20OR%20preSaleStartDate%20%3D%20-1%20OR%20preSaleEndDate%20%3D%20-1&facets=%5B%22preSale%22%5D&tagFilters=&facetFilters=%5B%22preSale%3Atrue%22%5D"}]}';
 $post_datas = curls($get_url, $json);
 $datas=json_decode($post_datas,true)['results'][0];
 $tmp=$datas['hits'];
