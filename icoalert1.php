50b5d6e7f33c796cec7b1e77fb5671fe809cb52e
932563595 <932563595@qq.com>
2018-04-11 06:19:07 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icoalert1.php

--- icoalert1.php
+++ icoalert1.php
@@ -18,7 +18,7 @@ function curls($url, $data_string) {
     return $data;
                 }
 $get_url="https://jbmtbl811x-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.21.1%3Binstantsearch.js%201.11.7%3BJS%20Helper%202.19.0&x-algolia-application-id=JBMTBL811X&x-algolia-api-key=bab8508db4e902d54d1603e9d897a285";
-$json='{"requests":[{"indexName":"icoalert-production-active","params":"query=&hitsPerPage=10000&page=0&filters=startDate%20%3C%3D%201522294754%20AND%20endDate%20%3E%201522294754%20AND%20startDate%20%3E%200&facets=%5B%5D&tagFilters="}]}';
+$json='{"requests":[{"indexName":"icoalert-production-active","params":"query=&hitsPerPage=10000&page=0&filters=startDate%20%3C%3D%20'.time().'%20AND%20endDate%20%3E%20'.time().'%20AND%20startDate%20%3E%200&facets=%5B%5D&tagFilters="}]}';
 $post_datas = curls($get_url, $json);
 $datas=json_decode($post_datas,true)['results'][0];
 $tmp=$datas['hits'];
