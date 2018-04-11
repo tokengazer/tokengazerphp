b141e85237dbca945c0e5a703572d9fc64f7090b
932563595 <932563595@qq.com>
2018-04-11 03:25:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getgithubdata1.php

--- getgithubdata1.php
+++ getgithubdata1.php
@@ -21,7 +21,7 @@ foreach($list as $k=>$v){
     while(1==1){
     $url="https://api.github.com/repos/".$results[$kk]['bitcoin/bips']."/contributors?page=".$i."&per_page=100";
         $res=json_decode(curls($url),true);
-        foreach($res $aa=>$bb){
+        foreach($res as $aa=>$bb){
         $commits+=$res[$aa]['contributions'];
         }
         if(count($res)<=100){
