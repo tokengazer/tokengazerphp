78d63b1931e16e40934e64499bf07aa3f47f4402
932563595 <932563595@qq.com>
2018-04-12 02:44:55 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getetherscandraw.php

--- getetherscandraw.php
+++ getetherscandraw.php
@@ -11,7 +11,7 @@ $ticker=explode(")",explode("(",$list[$k]['name'])[1])[0];
     $relist=explode(",",$re);
     foreach($relist as $kk=>$vv){
     $tmpname=explode(")",explode("(",$relist[$kk])[1])[0];
-        echo $rename=explode(" Token",explode("0x",$relist[$kk])[1])[0];
+        echo $rename=explode(" TOKEN",explode("0x",$relist[$kk])[1])[0];
         if(strtoupper($tmpname)==strtoupper($ticker)&&strtoupper($name)==strtoupper($rename)){
         $token=explode("\\",explode("\\t",$relist[$kk])[1])[0];
             $url1="https://etherscan.io/token/tokenholderchart/".$token."?range=100";
