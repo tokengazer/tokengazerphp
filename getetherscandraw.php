d34fcfb4428673281186a8cf706ccd143a427464
932563595 <932563595@qq.com>
2018-04-12 01:54:24 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getetherscandraw.php

--- getetherscandraw.php
+++ getetherscandraw.php
@@ -5,7 +5,7 @@ $list=MySQLGetData($sql);
 foreach($list as $k=>$v){
 $ticker=explode("",explode("(",$list[$k]['name'])[1])[0];
     $url="https://etherscan.io/searchHandler?term=".$ticker;
-    $re=file_get_contents_https($url);
+    $re=curls($url);
     echo print_r($re);
 }
 die;
@@ -25,4 +25,29 @@ foreach($jsonarr as $k=>$v){
 echo $sql="insert into etherscan_draw values ('1','".$v[0]."','".addslashes($v[1])."')";
     MySQLRunSQL($sql);
 }
+function curls($url){
+    $headers = array(
+        'accept: application/json, text/javascript, */*; q=0.01',
+       
+    );
+    $curl = curl_init();
+    //设置抓取的url
+    curl_setopt($curl, CURLOPT_URL, $url);
+    //curl_setopt($curl, CURLOPT_POST, 1);
+    //设置头文件的信息作为数据流输出
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    //设置获取的信息以文件流的形式返回，而不是直接输出。
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    //执行命令
+    $data = curl_exec($curl);
+    //关闭URL请求
+    curl_close($curl);
+    //显示获得的数据
+    if (substr($data, 0,3) == pack("CCC",0xef,0xbb,0xbf)) {
+        $data = substr($data, 3);
+    }
+    return $data;
+  }
 ?>
