d7e4f5596f1306a163c4adff807844a335c981dc
932563595 <932563595@qq.com>
2018-04-12 08:12:24 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/updatejiaoyidata.php

--- updatejiaoyidata.php
+++ updatejiaoyidata.php
@@ -34,7 +34,7 @@ foreach($list as $k=>$v){
     //echo $sql="update ico_Analysis set Github_url='".$githuburl."' where name='".$data[$k]['name']."'";
     //MySQLRunSQL($sql);
     if($urrent_Cirulation!=''){
-    $sql="update ico_Analysis set Current_market_value='".$Current_market_value."',Current_Circulation='".$Current_Circulation."',Current_Single_price='".$Current_Single_price."'  where id='".$list[$k]['id']."'";
+    echo $sql="update ico_Analysis set Current_market_value='".$Current_market_value."',Current_Circulation='".$Current_Circulation."',Current_Single_price='".$Current_Single_price."'  where id='".$list[$k]['id']."'";
     MySQLRunSQL($sql);
     }
     
