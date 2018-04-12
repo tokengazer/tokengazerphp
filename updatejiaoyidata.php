b762ef374fcabe0bfd0c69d5a0779fc5331ab3fe
932563595 <932563595@qq.com>
2018-04-12 06:17:51 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/updatejiaoyidata.php

---
+++ updatejiaoyidata.php
@@ -0,0 +1,66 @@
+<?php
+include('bootstraps.php');
+$sql="select * from ico_Analysis where DataSource='icorating'";
+$list=MySQLGetData($sql);
+foreach($list as $k=>$v){
+    $name=trim(explode("(",$list[$k]['name'])[0]);
+$ticker=explode(")",explode("(",$list[$k]['name'])[1])[0];
+    $url2="https://api.coinmarketcap.com/v1/ticker/".$name."/";
+    $data=curl2($url2);
+           print_r($data);
+    
+}
+die;
+
+function curls($url){
+    $headers = array(
+        'accept: application/json, text/javascript, */*; q=0.01',
+       
+    );
+    $curl = curl_init();
+    //设置抓取的url
+    curl_setopt($curl, CURLOPT_URL, $url);
+    //curl_setopt($curl, CURLOPT_POST, 1);
+    //设置头文件的信息作为数据流输出
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    //设置获取的信息以文件流的形式返回，而不是直接输出。
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    //执行命令
+    $data = curl_exec($curl);
+    //关闭URL请求
+    curl_close($curl);
+    //显示获得的数据
+    if (substr($data, 0,3) == pack("CCC",0xef,0xbb,0xbf)) {
+        $data = substr($data, 3);
+    }
+    return $data;
+  }
+function object_to_array($obj) {
+    $obj = (array)$obj;
+    foreach ($obj as $k => $v) {
+        if (gettype($v) == 'resource') {
+            return;
+        }
+        if (gettype($v) == 'object' || gettype($v) == 'array') {
+            $obj[$k] = (array)object_to_array($v);
+        }
+    }
+ 
+    return $obj;
+}
+function array_to_object($arr) {
+    if (gettype($arr) != 'array') {
+        return;
+    }
+    foreach ($arr as $k => $v) {
+        if (gettype($v) == 'array' || getType($v) == 'object') {
+            $arr[$k] = (object)array_to_object($v);
+        }
+    }
+ 
+    return (object)$arr;
+}
+
+?>
