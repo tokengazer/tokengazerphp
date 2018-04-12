c54bc9540ce46e19a4206e4fde5bf762124ce04c
932563595 <932563595@qq.com>
2018-04-12 01:43:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getetherscandraw.php

--- getetherscandraw.php
+++ getetherscandraw.php
@@ -1,5 +1,14 @@
 <?php
 include('bootstraps.php');
+$sql="select * from ico_Analysis ";
+$list=MySQLGetData($sql);
+foreach($list as $k=>$v){
+$ticker=explode("",explode("(",$list[$k]['name'])[1])[0];
+    $url="https://etherscan.io/searchHandler?term=".$ticker;
+    $re=file_get_contents_https($url);
+    echo $re;
+}
+die;
 $url="https://etherscan.io/token/tokenholderchart/0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0?range=100";
     $html=file_get_contents_https($url);
 
