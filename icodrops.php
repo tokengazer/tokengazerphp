568351a2f4fe64dba35b9d6546573c441f7a1f89
932563595 <932563595@qq.com>
2018-04-13 03:41:16 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icodrops.php

--- icodrops.php
+++ icodrops.php
@@ -8,7 +8,7 @@ $content = file_get_contents_https($url);
 $str1 = trim(getSonString($content, "<h3 class=\"col-md-12 col-12 not_rated\">All</h3>", "<div class=\"tabs__content\">"));
 $str2=explode("<div class=\"ico-main-info\">",$str1);
 unset($str2[0]);
-$i=0;
+$i=$j=0;
 foreach($str2 as $k=>$v){
 	$url=explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[0];
     $data['logo']=explode("\"",explode("data-src=\"",$str2[$k])[1])[0];
@@ -22,7 +22,7 @@ foreach($str2 as $k=>$v){
     MySQLRunSQL($sql);
      $kv->get('icodropsproducts:'.$i);
     }
-    $i++;
+    $i++;$j++;
     
 }
 
@@ -35,17 +35,19 @@ unset($str2[0]);
 foreach($str2 as $k=>$v){
 	$url=explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[0];
     $data['logo']=explode("\"",explode("data-src=\"",$str2[$k])[1])[0];
-    $arr[$i]['name']=$name=explode("</a>",explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[1])[0];
+    $arr[$j]['name']=$name=explode("</a>",explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[1])[0];
     $contents1=file_get_contents_https($url);
-    $arr[$i]['githuburl']=$githuburl="https://github.com/".explode("\"",explode("https://github.com/",$contents1)[1])[0];
+    $arr[$j]['githuburl']=$githuburl="https://github.com/".explode("\"",explode("https://github.com/",$contents1)[1])[0];
     $i++;
-    if($arr[$i]['name']!=''){
+    if($arr[$j]['name']!=''){
     $ret = $kv->delete('icodropsproducts:'.$i);
         $kv->add('icodropsproducts:'.$i, json_encode($arr[$i],true));
-    $sql='insert into ico_Analysis (name,logo,Github_url,DataSource) values("'.$arr[$i]['name'].'","'.$data['logo'].'","'.$arr[$i]['githuburl'].'","icodrops");';
+    $sql='insert into ico_Analysis (name,logo,Github_url,DataSource) values("'.$arr[$j]['name'].'","'.$data['logo'].'","'.$arr[$j]['githuburl'].'","icodrops");';
     MySQLRunSQL($sql);
-     $kv->get('icodropsproducts:'.$i);
+     $kv->get('icodropsproducts:'.$j);
+        
     }
+    $j++;
 }
 $i=0;
 $url = 'https://icodrops.com/category/ended-ico/';
@@ -57,17 +59,19 @@ unset($str2[0]);
 foreach($str2 as $k=>$v){
 	$url=explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[0];
     $data['logo']=explode("\"",explode("data-src=\"",$str2[$k])[1])[0];
-    $arr[$i]['name']=$name=explode("</a>",explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[1])[0];
+    $arr[$j]['name']=$name=explode("</a>",explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[1])[0];
     $contents1=file_get_contents_https($url);
-    $arr[$i]['githuburl']=$githuburl="https://github.com/".explode("\"",explode("https://github.com/",$contents1)[1])[0];
+    $arr[$j]['githuburl']=$githuburl="https://github.com/".explode("\"",explode("https://github.com/",$contents1)[1])[0];
     $i++;
-    if($arr[$i]['name']!=''){
+    if($arr[$j]['name']!=''){
     $ret = $kv->delete('icodropsproducts:'.$i);
         $kv->add('icodropsproducts:'.$i, json_encode($arr[$i],true));
-    $sql='insert into ico_Analysis (name,logo,Github_url,DataSource) values("'.$arr[$i]['name'].'","'.$data['logo'].'","'.$arr[$i]['githuburl'].'","icodrops");';
+    $sql='insert into ico_Analysis (name,logo,Github_url,DataSource) values("'.$arr[$j]['name'].'","'.$data['logo'].'","'.$arr[$j]['githuburl'].'","icodrops");';
     MySQLRunSQL($sql);
-     $kv->get('icodropsproducts:'.$i);
+     $kv->get('icodropsproducts:'.$j);
+        
     }
+    $j++;
 }
 $ret = $kv->delete('icodropsproducts:all');
         $kv->add('icodropsproducts:all', json_encode($arr,true));
