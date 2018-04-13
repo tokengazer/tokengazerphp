f4f6928e7ec60563fc069f997d4bd09dbb648c4b
932563595 <932563595@qq.com>
2018-04-13 05:58:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/icodrops.php

--- icodrops.php
+++ icodrops.php
@@ -12,14 +12,21 @@ $i=$j=0;
 foreach($str2 as $k=>$v){
 	$url=explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[0];
     $data['logo']=explode("\"",explode("data-src=\"",$str2[$k])[1])[0];
+    $data[$k]['Ico_Raise_money']=trim(explode("</div>",explode("<div id='categ_desctop'>",$str2[$k])[1])[0]);
     $arr[$i]['name']=$name=explode("</a>",explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[1])[0];
     $contents1=file_get_contents_https($url);
     $arr[$i]['githuburl']=$githuburl="https://github.com/".explode("\"",explode("https://github.com/",$contents1)[1])[0];
     if($arr[$i]['name']!=''){
     $ret = $kv->delete('icodropsproducts:'.$i);
         $kv->add('icodropsproducts:'.$i, json_encode($arr[$i],true));
+        if(count(MySQLGetData($sql))>=1){
+        $sql="update ico_Analysis set ICO_Raise_money='".$data[$k]['Ico_Raise_money']."' where name='".$name."'";
+        }
+        else{
     $sql='insert into ico_Analysis (name,logo,Github_url,DataSource) values("'.$arr[$i]['name'].'","'.$data['logo'].'","'.$arr[$i]['githuburl'].'","icodrops");';
-    MySQLRunSQL($sql);
+    
+        }
+        MySQLRunSQL($sql);
      $kv->get('icodropsproducts:'.$i);
     }
     $i++;$j++;
@@ -40,10 +47,18 @@ foreach($str2 as $k=>$v){
     $arr[$j]['githuburl']=$githuburl="https://github.com/".explode("\"",explode("https://github.com/",$contents1)[1])[0];
     $i++;
     if($arr[$j]['name']!=''){
+        $sql="select * from ico_Analysis where name ='".$arr[$j]['name']."'";
+        if(count(MySQLGetData($sql))>=1){
+        
+        }
+        else{
+        $sql='insert into ico_Analysis (name,logo,Github_url,DataSource) values("'.$arr[$j]['name'].'","'.$data['logo'].'","'.$arr[$j]['githuburl'].'","icodrops");';
+     MySQLRunSQL($sql);
+        }
     $ret = $kv->delete('icodropsproducts:'.$i);
         $kv->add('icodropsproducts:'.$i, json_encode($arr[$i],true));
     $sql='insert into ico_Analysis (name,logo,Github_url,DataSource) values("'.$arr[$j]['name'].'","'.$data['logo'].'","'.$arr[$j]['githuburl'].'","icodrops");';
-    MySQLRunSQL($sql);
+   
      $kv->get('icodropsproducts:'.$j);
         
     }
