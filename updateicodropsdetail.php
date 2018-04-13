b454a80d48c87ea26bbcd2605a0893967b59abc0
932563595 <932563595@qq.com>
2018-04-13 07:04:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/updateicodropsdetail.php

--- updateicodropsdetail.php
+++ updateicodropsdetail.php
@@ -3,7 +3,7 @@ include('bootstraps.php');
 $sql="select * from ico_Analysis where DataSource='icodrops'";
 $results=MySQLGetData($sql);
 foreach($results as $k=>$v){
-$url="https://icodrops.com/".$results[$k]['name']."/";
+$url="https://icodrops.com/".str_replace(" ","-",trim($results[$k]['name']))."/";
     $html=file_get_contents_https($url);
     $websitetmpstr=explode("\" target=\"_blank\" rel=\"nofollow\"><div class=\"button\" >WEBSITE",$html)[0];
     echo $website=explode("<a href=\"",$websitetmpstr)[count(explode("<a href=\"",$websitetmpstr))-1];
