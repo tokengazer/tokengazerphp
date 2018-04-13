5bfa013e214306e77bbe26ffe80c172513ff2faf
932563595 <932563595@qq.com>
2018-04-13 08:18:49 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/updateicodropsdetail.php

--- updateicodropsdetail.php
+++ updateicodropsdetail.php
@@ -14,5 +14,15 @@ $url="https://icodrops.com/".str_replace(" ","-",trim($results[$k]['name']))."/"
     $website=explode("<a href=\"",$websitetmpstr)[count(explode("<a href=\"",$websitetmpstr))-1];
     $whitepapertmpstr=explode("\" target=\"_blank\" rel=\"nofollow\"><div class=\"button\" >WHITEPAPER",$html)[0];
     echo $whitepaper=explode("<a href=\"",$whitepapertmpstr)[count(explode("<a href=\"",$whitepapertmpstr))-1];
+    $saletime=explode("<i class=\"fa fa-calendar\" aria-hidden=\"true\"></i>",$html)[1];
+    $saletime=trim(explode("</h4>",explode("</h4>",$saletme)[0])[1]);
+    $saletime=trim(str_replace("Token Sale:","",$saletime));
+    $icostartdate=explode("-",$saletime)[0];
+    $icoenddate=explode("-",$saletime)[1];
+    $ticker=explode("</li>",explode("Ticker: </span>",$html)[1])[0];
+    $whitelist=explode(",",explode("Whitelist: </span>",$html)[1])[0];
+    $cannotareas=explode("</li>",explode("Сan't participate: </span>",$html)[1])[0];
+    $platform=explode("</li>",explode("Accepts: </span>",$html)[1])[0];
+    
 }
 ?>
