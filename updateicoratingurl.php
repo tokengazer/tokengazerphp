a7e03f04fc52be525d560d8d0c33486efcf475dc
932563595 <932563595@qq.com>
2018-04-16 09:49:02 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/updateicoratingurl.php

--- updateicoratingurl.php
+++ updateicoratingurl.php
@@ -17,7 +17,7 @@ $trlist=explode("<tr data-href='",$str1);
 $urllist=array();
 foreach($trlist as $kk=>$vv){
 $urllist[$kk]=explode("'",$trlist[$kk])[0];
-}echo count($urllist);die;
+}
 //print_r($str1);
 $str2=explode('<td>',$str1);
 $head2 = '<h2>Unassessed</h2>';
