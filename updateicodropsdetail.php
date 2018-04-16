c8e17aefa5e8b3d0930272b8b0ad26f8a37862fd
932563595 <932563595@qq.com>
2018-04-16 07:03:40 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/updateicodropsdetail.php

--- updateicodropsdetail.php
+++ updateicodropsdetail.php
@@ -13,6 +13,7 @@ $url="https://icodrops.com/".str_replace(" ","-",trim($results[$k]['name']))."/"
     $websitetmpstr=explode("\" target=\"_blank\" rel=\"nofollow\"><div class=\"button\" >WEBSITE",$html)[0];
     $website=explode("<a href=\"",$websitetmpstr)[count(explode("<a href=\"",$websitetmpstr))-1];
     $whitepapertmpstr=explode("\" target=\"_blank\" rel=\"nofollow\"><div class=\"button\" >WHITEPAPER",$html)[0];
+    $githuburl="https://github.com/".explode("\"",explode("github.com/",$html)[1])[0;]
     echo $whitepaper=explode("<a href=\"",$whitepapertmpstr)[count(explode("<a href=\"",$whitepapertmpstr))-1];
     $saletime=explode("<i class=\"fa fa-calendar\" aria-hidden=\"true\"></i>",$html)[1];
     $saletime=trim(explode("</h4>",explode("</h4>",$saletme)[0])[1]);
@@ -23,7 +24,7 @@ $url="https://icodrops.com/".str_replace(" ","-",trim($results[$k]['name']))."/"
     $whitelist=explode(",",explode("Whitelist: </span>",$html)[1])[0];
     $cannotareas=explode("</li>",explode("Сan't participate: </span>",$html)[1])[0];
     $platform=explode("</li>",explode("Accepts: </span>",$html)[1])[0];
-    $sql="update icco_Analysis set whitepaper='".$whitepaper."',ticker='".$ticker."',whitelist='".$whitelist."',Ico_time='".$icostartdate."',Platform='".$platform."',cannotareas='".$cannotareas."',website='".$website."' where id=".$$results[$k]['id']."";
+    $sql="update icco_Analysis set Github_url='".$githuburl."',whitepaper='".$whitepaper."',ticker='".$ticker."',whitelist='".$whitelist."',Ico_time='".$icostartdate."',Platform='".$platform."',cannotareas='".$cannotareas."',website='".$website."' where id=".$$results[$k]['id']."";
     MySQLRunSQL($sql);
 }
 ?>
