167894b5331a2637e2f3c60125111b0dabb698b8
932563595 <932563595@qq.com>
2018-04-16 06:51:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticodatas.php

--- geticodatas.php
+++ geticodatas.php
@@ -4,8 +4,9 @@ echo $sql="select name from ico_Analysis where id>396";
 $data=MySQLGetData($sql);
 foreach($data as $k=>$v){
 $url="https://api.coinmarketcap.com/v1/ticker/".$data[$k]['name']."/";
-    $results=json_decode(file_get_contents_https($url),true);
-    if(isset($results['error'])){
+    $result=file_get_contents_https($url);
+    $results=json_decode(result,true);
+    if(isset($results['error'])&&is_not_json($result)==false){
     continue;
     }
     $Current_market_value=$results[0]['market_cap_usd'];
@@ -21,5 +22,7 @@ $url="https://api.coinmarketcap.com/v1/ticker/".$data[$k]['name']."/";
     $sql="update ico_Analysis set Current_market_value='".$Current_market_value."',Current_Circulation='".$Current_Circulation."',Current_Single_price='".$Current_Single_price."'  where name='".$data[$k]['name']."'";
     MySQLRunSQL($sql);
 }
-
+function is_not_json($str){ 
+    return is_null(json_decode($str));
+}
 ?>
