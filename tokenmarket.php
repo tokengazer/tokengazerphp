0377c644bc6d56cc99bcd0d4fa6a56d5248e39d7
932563595 <932563595@qq.com>
2018-04-17 07:13:04 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tokenmarket.php

--- tokenmarket.php
+++ tokenmarket.php
@@ -68,7 +68,7 @@ unset($tmp[0]);$i=1;
         }
         //$sql="select * from ico_Analysis where name='".$data[$i]['name']."'";
         //$has=MySQLGetData($sql);
-        }
+        }echo 1;
         if(count($has)==0){
         //没有就插入
         $sql='insert into ico_Analysis (name,Github_url,DataSource,Ico_time,Team,origin,whitepaper,website,logo,linkedin,icolink) values("'.$data[$i]['name'].'","'.$data[$i]['githuburl'].'","tokenmarket","'.$icostartdate.'","'.$member.'","'.$origin.'","'.$whitepaper.'","'.$website1.'","'.$logo.'","'.$linkedinurl.'","'.$url.'");';
