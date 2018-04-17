30c3c2a77644879c9621dc28cf6526194e789f8f
932563595 <932563595@qq.com>
2018-04-17 09:00:51 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/coingeckolist.php

--- coingeckolist.php
+++ coingeckolist.php
@@ -2,7 +2,7 @@
 include('bootstraps.php');
 $url="https://www.coingecko.com/zh/ico";
 $html=file_get_contents_https($url);
-$tophtml=explode("<tr class='asset-content sponsored'>",$html);
+$tophtml=explode('<tr class=\'asset-content sponsored\'>',$html);
 
 print_r($tophtml);
 ?>
