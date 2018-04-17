e0bcdb40569d45cc07b87487b73b57eea49e6ace
932563595 <932563595@qq.com>
2018-04-17 02:41:12 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/update500githuburl.php

--- update500githuburl.php
+++ update500githuburl.php
@@ -20,11 +20,11 @@ $arr=array();
 foreach($url1 as $k=>$v){
     set_time_limit();
 //$contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
-    echo "https://coinmarketcap.com/currencies/".$url1[$k]['name'].'/';
+    "https://coinmarketcap.com/currencies/".$url1[$k]['name'].'/';
     $contents1=file_get_contents_https("https://coinmarketcap.com/currencies/".$url1[$k]['name'].'/');
     $arr[$i]['name']=$url1[$k]['id'];//echo "https://coinmarketcap.com/currencies/".$url1[$k]['id'];
     //print_r($contents1);die;
-    $arr[$i]['githuburl']=$githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','" target="');
+    echo $arr[$i]['githuburl']=$githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','" target="');
     $kv->delete('products:'.$i);
     $kv->add('products:'.$i, json_encode($arr[$i],true));
     $kv->get('products:'.$i);
