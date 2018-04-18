9d7a79d5445c66de1283b73be5963c5548e7a45d
932563595 <932563595@qq.com>
2018-04-18 06:55:25 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticoratingmember.php

--- geticoratingmember.php
+++ geticoratingmember.php
@@ -17,7 +17,7 @@ MySQLRunSQL($sql);
     continue;
     }
     foreach($tmparr as $kk=>$vv){
-    $data[$k]['mamber'][$kk]['headimg']=explode("\"",explode("src=\"",$tmparr[$kk])[1])[0];
+    echo $data[$k]['mamber'][$kk]['headimg']=explode("\"",explode("src=\"",$tmparr[$kk])[1])[0];
         $data[$k]['mamber'][$kk]['name']=explode("\"",explode("<a title=\"",$tmparr[$kk])[1])[0];
         $data[$k]['member'][$kk]['linkedin']=explode("\"",explode("https://www.linkedin.com",$tmparr[$kk])[1])[0];
         echo $sql="select * from TeamMember where name='".$data[$k]['member'][$kk]['name']."' and pid=$id";
