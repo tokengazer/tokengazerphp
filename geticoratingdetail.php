97e23543f9681ac7346ce3d6a6a7d4d3ebcb0562
932563595 <932563595@qq.com>
2018-04-18 03:03:56 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticoratingdetail.php

--- geticoratingdetail.php
+++ geticoratingdetail.php
@@ -81,13 +81,13 @@ $ICOPlatform=getSonString($str,"<td>","</td>");
     
 $str1=explode("<td>Registration Country:</td>",$html1)[1];
     $str =explode("uk-table",$html)[4];
-$regin=explode("</td>",explode("<td>",$str1)[1])[0];print_r($regin);die;
+$regin=explode("</td>",explode("<td>",$str1)[1])[0];
     $str =explode("uk-table",$html)[4];
-$str=explode("Registration Year:",$str)[1];
-$regyear=getSonString($str,"<td>","</td>");
+$str1=explode("<td>Registration Year:</td>",$html1)[1];
+$regyear=explode("</td>",explode("<td>",$str1)[1])[0];
     $str =explode("uk-table",$html)[4];
-$str=explode("Office adress:",$str)[1];
-$regin.=".".getSonString($str,"<td>","</td>");
+$str1=explode("<td>Office adress:</td>",$html1)[1];
+$regin.=".".=explode("</td>",explode("<td>",$str1)[1])[0];;
  $ICOPlatform;
 $str =explode("uk-table",$html)[5];
 $str=explode("Bug Detection:",$str)[1];
