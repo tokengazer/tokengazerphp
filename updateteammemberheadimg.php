71c9af6c34fc378292ecdb5bc35ab7ffced9f572
932563595 <932563595@qq.com>
2018-04-18 07:49:08 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/updateteammemberheadimg.php

--- updateteammemberheadimg.php
+++ updateteammemberheadimg.php
@@ -4,7 +4,7 @@ $sql="select headimg from TeamMember";
 $data=MySQLGetData($sql);
 foreach($data as $kk=>$vv){
 if(strpos($data[$kk]['headimg'],'https://icorating.com') === false){
-$sql="update TeamMember set headimg='https://icorating.cm'".$data[$kk]['headimg']." where id=".$data[$kk]['id'].";";
+$sql="update TeamMember set headimg='https://icorating.com'".$data[$kk]['headimg']." where id=".$data[$kk]['id'].";";
     MySQLRunSQL($sql);
 }
 }
