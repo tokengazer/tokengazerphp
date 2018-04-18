fa8063b3ee06e917cd857f3aa4a26cf5ff3e5f40
932563595 <932563595@qq.com>
2018-04-18 14:16:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticoratingdetail.php

--- geticoratingdetail.php
+++ geticoratingdetail.php
@@ -3,7 +3,7 @@ include('bootstraps.php');
 $sql="select * from ico_Analysis where name <>'' and DataSource='icorating'";
 $data=MySQLGetData($sql);
 foreach($data as $k=>$v){
- 
+ print_r($data);
     $url=$data[$k]['icolink'];
 echo 1;
 //$url="https://icorating.com/ico/crowd-machine/details/";
