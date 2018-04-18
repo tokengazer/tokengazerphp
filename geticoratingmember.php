95ad7fdb1e59b48f3770997584d95aa338ce0c54
932563595 <932563595@qq.com>
2018-04-18 06:27:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticoratingmember.php

---
+++ geticoratingmember.php
@@ -0,0 +1,19 @@
+<?php
+include('bootstraps.php');
+$sql="select * from ico_Analysis where name <>'' and DataSource='icorating' and id=1296";
+$data=MySQLGetData($sql);
+foreach($data as $k=>$v){
+ $url=$data[$k]['icolink'];
+
+//$url="https://icorating.com/ico/crowd-machine/details/";
+    
+$html1=$html=file_get_contents_https($url.'team/');
+MySQLRunSQL($sql);
+    $founders=explode("<tbody>",$html1)[1];
+    foreach($founders as $kk=>$vv){
+    $tmparr=explode("<tr>",$founders[$kk]);
+        unset($tmparr);
+        print_r($tmparr);
+    }
+}
+?>
