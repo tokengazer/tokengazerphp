1aafb870f19f7a08a0a272a4b6500e18200bda25
932563595 <932563595@qq.com>
2018-04-18 06:50:32 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticoratingmember.php

--- geticoratingmember.php
+++ geticoratingmember.php
@@ -4,6 +4,7 @@ $sql="select * from ico_Analysis where name <>'' and DataSource='icorating' and 
 $data=MySQLGetData($sql);
 foreach($data as $k=>$v){
  $url=$data[$k]['icolink'];
+    $id=$data[$k]['id'];
 
 //$url="https://icorating.com/ico/crowd-machine/details/";
     
@@ -12,8 +13,38 @@ MySQLRunSQL($sql);
     $founders=explode("<tbody>",$html1)[1];
    $tmparr=explode("<tr>",$founders);
     unset($tmparr[0]);
+    if(count($tmparr)==0){
+    continue;
+    }
     foreach($tmparr as $kk=>$vv){
-    print_r($tmparr);
+    $data[$k]['mamber'][$kk]['headimg']=explode("\"",explode("src=\"",$tmparr[$kk])[1])[0];
+        $data[$k]['mamber'][$kk]['name']=explode("\"",explode("<a title=\"Stepan",$tmparr[$kk])[1])[0];
+        $data[$k]['member'][$kk]['linkedin']=explode("\"",explode("https://www.linkedin.com",$tmparr[$kk])[1])[0];
+        $sql="select * from TeamMember where name='".$data[$k]['member'][$kk]['name']."' and pid=$id";
+        if(count(MySQLGetData($sql))>=0){
+        
+        }else{
+        $sql="insert into TeamMember values(NULL,'".$data[$k]['mamber'][$kk]['name']."','".$data[$k]['mamber'][$kk]['headimg']."','".$data[$k]['member'][$kk]['linkedin']."','founder')";
+            MySQLRunSQL($sql);
+        }
+    }
+    $founders=explode("<tbody>",$html1)[2];
+   $tmparr=explode("<tr>",$founders);
+    unset($tmparr[0]);
+    if(count($tmparr)==0){
+    continue;
+    }
+    foreach($tmparr as $kk=>$vv){
+    $data[$k]['mamber'][$kk]['headimg']=explode("\"",explode("src=\"",$tmparr[$kk])[1])[0];
+        $data[$k]['mamber'][$kk]['name']=explode("\"",explode("<a title=\"Stepan",$tmparr[$kk])[1])[0];
+        $data[$k]['member'][$kk]['linkedin']=explode("\"",explode("https://www.linkedin.com",$tmparr[$kk])[1])[0];
+        $sql="select * from TeamMember where name='".$data[$k]['member'][$kk]['name']."' and pid=$id";
+        if(count(MySQLGetData($sql))>=0){
+        
+        }else{
+        $sql="insert into TeamMember values(NULL,'".$data[$k]['mamber'][$kk]['name']."','".$data[$k]['mamber'][$kk]['headimg']."','".$data[$k]['member'][$kk]['linkedin']."','Advisors')";
+            MySQLRunSQL($sql);
+        }
     }
 }
 ?>
