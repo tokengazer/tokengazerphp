b195c80c9351a9b7ec4bfdaa0640b8d5feb612ee
932563595 <932563595@qq.com>
2018-04-18 06:58:04 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/geticoratingmember.php

--- geticoratingmember.php
+++ geticoratingmember.php
@@ -21,7 +21,7 @@ MySQLRunSQL($sql);
         $data[$k]['member'][$kk]['name']=explode("\"",explode("<a title=\"",$tmparr[$kk])[1])[0];
         $data[$k]['member'][$kk]['linkedin']=explode("\"",explode("https://www.linkedin.com",$tmparr[$kk])[1])[0];
         echo $sql="select * from TeamMember where name='".$data[$k]['member'][$kk]['name']."' and pid=$id";
-        if(count(MySQLGetData($sql))>=0){
+        if(count(MySQLGetData($sql))>0){
         
         }else{
         echo $sql="insert into TeamMember values(NULL,'".$data[$k]['member'][$kk]['name']."','".$data[$k]['member'][$kk]['headimg']."','".$data[$k]['member'][$kk]['linkedin']."','founder')";
@@ -35,14 +35,14 @@ MySQLRunSQL($sql);
     continue;
     }
     foreach($tmparr as $kk=>$vv){
-    $data[$k]['mamber'][$kk]['headimg']=explode("\"",explode("src=\"",$tmparr[$kk])[1])[0];
-        $data[$k]['mamber'][$kk]['name']=explode("\"",explode("<a title=\"Stepan",$tmparr[$kk])[1])[0];
+    $data[$k]['member'][$kk]['headimg']=explode("\"",explode("src=\"",$tmparr[$kk])[1])[0];
+        $data[$k]['member'][$kk]['name']=explode("\"",explode("<a title=\"Stepan",$tmparr[$kk])[1])[0];
         $data[$k]['member'][$kk]['linkedin']=explode("\"",explode("https://www.linkedin.com",$tmparr[$kk])[1])[0];
         $sql="select * from TeamMember where name='".$data[$k]['member'][$kk]['name']."' and pid=$id";
-        if(count(MySQLGetData($sql))>=0){
+        if(count(MySQLGetData($sql))>0){
         
         }else{
-        $sql="insert into TeamMember values(NULL,'".$data[$k]['mamber'][$kk]['name']."','".$data[$k]['mamber'][$kk]['headimg']."','".$data[$k]['member'][$kk]['linkedin']."','Advisors')";
+        $sql="insert into TeamMember values(NULL,'".$data[$k]['member'][$kk]['name']."','".$data[$k]['member'][$kk]['headimg']."','".$data[$k]['member'][$kk]['linkedin']."','Advisors')";
             MySQLRunSQL($sql);
         }
     }
