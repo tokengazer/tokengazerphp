16685b15a36ad5f03726d8517e48c8e5a2bfdd64
932563595 <932563595@qq.com>
2018-04-19 09:48:10 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tocsv.php

--- tocsv.php
+++ tocsv.php
@@ -11,13 +11,72 @@ if(isset($_GET['name'])) {
 
 }
 $data=MySQLGetData($sql);
+print_r($data);die;
 $cells="logo,name,ticker,DataSource,Current_market_value,Current_Single_Price,Current_Circulation,Circulation_unit,Total_Count,Twitter_Fanscount,Facebook_Friends,Telegram_fans,Github_url,GithubCommits,GithubStars,GithubWatches,GithubForks,Github_lastupdatetime,Ico_time,ICO_Price_Usd,ICO_Price_ETH,ICO_Distribution_Ratio,Presales,ICO_Total_Amount,ICO_TotalCount,ICO_HardCap,ICO_Raise_money,Business,Technology,Team,Token,Operation,members,origin,whitepaper,website,cannotareas,Platform,icolink,linkedin";
 $cell=explode(',',$cells);
 $engcells=explode(",","logo,name,ticker,DataSource,Current_market_value,Current_Single_Price,Current_Circulation,Circulation_unit,Total_Count,Twitter_Fanscount,Facebook_Friends,Telegram_fans,Github_url,GithubCommits,GithubStars,GithubWatches,GithubForks,Github_lastupdatetime,Ico_time,ICO_Price_Usd,ICO_Price_ETH,ICO_Distribution_Ratio,Presales,ICO_Total_Amount,ICO_TotalCount,ICO_HardCap,ICO_Raise_money,Business,Technology,Team,Token,Operation,members,origin,whitepaper,website,cannotareas,Platform,icolink,linkedin");
 
-export($name.date("Y-m-d")."csv",$cell,$data,$engcells);
+exportExcel($name.date("Y-m-d")."csv",$cell,$data);
 
-/*function exportExcel($expTitle,$expCellName,$expTableData,$cell=''){
+
+
+
+//简单过滤
+function strFilter($str){
+    $str = str_replace('`', '', $str);
+    $str = str_replace('·', '', $str);
+    $str = str_replace('~', '', $str);
+    $str = str_replace('!', '', $str);
+    $str = str_replace('！', '', $str);
+    $str = str_replace('@', '', $str);
+    $str = str_replace('#', '', $str);
+    $str = str_replace('$', '', $str);
+    $str = str_replace('￥', '', $str);
+    $str = str_replace('%', '', $str);
+    $str = str_replace('^', '', $str);
+    $str = str_replace('……', '', $str);
+    $str = str_replace('&', '', $str);
+    $str = str_replace('*', '', $str);
+    $str = str_replace('(', '', $str);
+    $str = str_replace(')', '', $str);
+    $str = str_replace('（', '', $str);
+    $str = str_replace('）', '', $str);
+    $str = str_replace('-', '', $str);
+    $str = str_replace('_', '', $str);
+    $str = str_replace('——', '', $str);
+    $str = str_replace('+', '', $str);
+    $str = str_replace('=', '', $str);
+    $str = str_replace('|', '', $str);
+    $str = str_replace('\\', '', $str);
+    $str = str_replace('[', '', $str);
+    $str = str_replace(']', '', $str);
+    $str = str_replace('【', '', $str);
+    $str = str_replace('】', '', $str);
+    $str = str_replace('{', '', $str);
+    $str = str_replace('}', '', $str);
+    $str = str_replace(';', '', $str);
+    $str = str_replace('；', '', $str);
+    $str = str_replace(':', '', $str);
+    $str = str_replace('：', '', $str);
+    $str = str_replace('\'', '', $str);
+    $str = str_replace('"', '', $str);
+    $str = str_replace('“', '', $str);
+    $str = str_replace('”', '', $str);
+    $str = str_replace(',', '', $str);
+    $str = str_replace('，', '', $str);
+    $str = str_replace('<', '', $str);
+    $str = str_replace('>', '', $str);
+    $str = str_replace('《', '', $str);
+    $str = str_replace('》', '', $str);
+    $str = str_replace('.', '', $str);
+    $str = str_replace('。', '', $str);
+    $str = str_replace('/', '', $str);
+    $str = str_replace('、', '', $str);
+    $str = str_replace('?', '', $str);
+    $str = str_replace('？', '', $str);
+    return trim($str);
+}
+function exportExcel($expTitle,$expCellName,$expTableData,$cell=''){
     $xlsTitle = iconv('utf-8', 'gb2312', $expTitle);//文件名称
     $fileName = $expTitle.date('_YmdHis');//or $xlsTitle 文件名称可根据自己情况设定
     $cellNum = count($expCellName);
@@ -49,36 +108,6 @@ export($name.date("Y-m-d")."csv",$cell,$data,$engcells);
     $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
     $objWriter->save('php://output');
     exit;
-}*/
-function export($title,$map,$data,$firstRow){
-    $objPHPExcel = new PHPExcel();
-    $objPHPExcel->getProperties()->setCreator('http://www.style.net')
-        ->setLastModifiedBy('http://www.style.net')
-        ->setTitle('Office 2007 XLSX Document')
-        ->setSubject('Office 2007 XLSX Document')
-        ->setDescription('Document for Office 2007 XLSX, generated using PHP classes.')
-        ->setKeywords('office 2007 openxml php')
-        ->setCategory('Result file');
-    foreach ($map as $k=>$v){
-        $objPHPExcel->getActiveSheet()->getColumnDimension($k)->setWidth(22);
-        $objPHPExcel->getActiveSheet()->getStyle($k.'1')->getFont()->setBold(true);
-        $objPHPExcel->getActiveSheet()->getStyle($k.'1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-    }
-    
-    $i = 2;
-    foreach ($data as $k=>$v) {
-        foreach ($map as $col=>$name) {
-            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($col . $i, $v[$name]);
-        }
-        $i++;
-    }
-    $objPHPExcel->getActiveSheet()->setTitle($title);
-    $objPHPExcel->setActiveSheetIndex(0);
-    $filename1 = urlencode('导出_'.$title) . '_' . date('Y-m-dHis');
-    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    header('Content-Disposition: attachment;filename="' . $filename1 . '.xlsx"');
-    header('Cache-Control: max-age=0');
-    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-    $objWriter->save('php://output');
 }
+
 ?>
