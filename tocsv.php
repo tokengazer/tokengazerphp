9f74a982e8d7543bd57b3ba2bcf822dc608fe076
932563595 <932563595@qq.com>
2018-04-19 09:48:29 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tocsv.php

--- tocsv.php
+++ tocsv.php
@@ -11,71 +11,12 @@ if(isset($_GET['name'])) {
 
 }
 $data=MySQLGetData($sql);
-print_r($data);die;
 $cells="logo,name,ticker,DataSource,Current_market_value,Current_Single_Price,Current_Circulation,Circulation_unit,Total_Count,Twitter_Fanscount,Facebook_Friends,Telegram_fans,Github_url,GithubCommits,GithubStars,GithubWatches,GithubForks,Github_lastupdatetime,Ico_time,ICO_Price_Usd,ICO_Price_ETH,ICO_Distribution_Ratio,Presales,ICO_Total_Amount,ICO_TotalCount,ICO_HardCap,ICO_Raise_money,Business,Technology,Team,Token,Operation,members,origin,whitepaper,website,cannotareas,Platform,icolink,linkedin";
 $cell=explode(',',$cells);
 $engcells=explode(",","logo,name,ticker,DataSource,Current_market_value,Current_Single_Price,Current_Circulation,Circulation_unit,Total_Count,Twitter_Fanscount,Facebook_Friends,Telegram_fans,Github_url,GithubCommits,GithubStars,GithubWatches,GithubForks,Github_lastupdatetime,Ico_time,ICO_Price_Usd,ICO_Price_ETH,ICO_Distribution_Ratio,Presales,ICO_Total_Amount,ICO_TotalCount,ICO_HardCap,ICO_Raise_money,Business,Technology,Team,Token,Operation,members,origin,whitepaper,website,cannotareas,Platform,icolink,linkedin");
 
 exportExcel($name.date("Y-m-d")."csv",$cell,$data);
 
-
-
-
-//简单过滤
-function strFilter($str){
-    $str = str_replace('`', '', $str);
-    $str = str_replace('·', '', $str);
-    $str = str_replace('~', '', $str);
-    $str = str_replace('!', '', $str);
-    $str = str_replace('！', '', $str);
-    $str = str_replace('@', '', $str);
-    $str = str_replace('#', '', $str);
-    $str = str_replace('$', '', $str);
-    $str = str_replace('￥', '', $str);
-    $str = str_replace('%', '', $str);
-    $str = str_replace('^', '', $str);
-    $str = str_replace('……', '', $str);
-    $str = str_replace('&', '', $str);
-    $str = str_replace('*', '', $str);
-    $str = str_replace('(', '', $str);
-    $str = str_replace(')', '', $str);
-    $str = str_replace('（', '', $str);
-    $str = str_replace('）', '', $str);
-    $str = str_replace('-', '', $str);
-    $str = str_replace('_', '', $str);
-    $str = str_replace('——', '', $str);
-    $str = str_replace('+', '', $str);
-    $str = str_replace('=', '', $str);
-    $str = str_replace('|', '', $str);
-    $str = str_replace('\\', '', $str);
-    $str = str_replace('[', '', $str);
-    $str = str_replace(']', '', $str);
-    $str = str_replace('【', '', $str);
-    $str = str_replace('】', '', $str);
-    $str = str_replace('{', '', $str);
-    $str = str_replace('}', '', $str);
-    $str = str_replace(';', '', $str);
-    $str = str_replace('；', '', $str);
-    $str = str_replace(':', '', $str);
-    $str = str_replace('：', '', $str);
-    $str = str_replace('\'', '', $str);
-    $str = str_replace('"', '', $str);
-    $str = str_replace('“', '', $str);
-    $str = str_replace('”', '', $str);
-    $str = str_replace(',', '', $str);
-    $str = str_replace('，', '', $str);
-    $str = str_replace('<', '', $str);
-    $str = str_replace('>', '', $str);
-    $str = str_replace('《', '', $str);
-    $str = str_replace('》', '', $str);
-    $str = str_replace('.', '', $str);
-    $str = str_replace('。', '', $str);
-    $str = str_replace('/', '', $str);
-    $str = str_replace('、', '', $str);
-    $str = str_replace('?', '', $str);
-    $str = str_replace('？', '', $str);
-    return trim($str);
-}
 function exportExcel($expTitle,$expCellName,$expTableData,$cell=''){
     $xlsTitle = iconv('utf-8', 'gb2312', $expTitle);//文件名称
     $fileName = $expTitle.date('_YmdHis');//or $xlsTitle 文件名称可根据自己情况设定
