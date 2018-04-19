69f871ba833eb78c4c09ef115fa7cebc6f66a931
932563595 <932563595@qq.com>
2018-04-19 03:09:48 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/searchapi.php

--- searchapi.php
+++ searchapi.php
@@ -1,14 +1,14 @@
 <?php
 include('bootstraps.php');
 $name=trim(strFilter($_POST['name']));
-$sql="select a.* from ico_Analysis as a ,b.name,b.pid,b.headimg,b.linkedinurl from TeamMember asb where a.name like '%$name%' and a.id=b.pid;";
+$sql="select a.*,b.name,b.headimg,b.linkedinurl,b.role from ico_Analysis as a left join TeamMember as b on a.id=b.pid where a.name like '%$name%' ;";
 $data=MySQLGetData($sql);
 if(count($data)==0){
 $re['erroCode']=-1;
     $re['count']=0;
 }else{
 $re['erroCode']=0;
-    $re['count']=counbt($data);
+    $re['count']=count($data);
     $re['data']=$data;
 }
 echo json_encode($re,true);
