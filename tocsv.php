97005d96b2f4a93026c306b5f04cf9c45c864bce
932563595 <932563595@qq.com>
2018-04-20 10:28:34 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tocsv.php

--- tocsv.php
+++ tocsv.php
@@ -11,11 +11,11 @@ include('bootstraps.php');
 }*/
 include("PHPExcel/PHPExcel.php");
 
-$data=MySQLGetData($sql);
+//$data=MySQLGetData($sql);
 $cells="id,name,logo,ticker,DataSource,Current_market_value,Current_Single_Price,Current_Circulation,Circulation_unit,Total_Count,Twitter_Fanscount,Facebook_Friends,Telegram_fans,Github_url,GithubCommits,GithubStars,GithubWatches,GithubForks,Github_lastupdatetime,Ico_time,ICO_Price_Usd,ICO_Price_ETH,ICO_Distribution_Ratio,Presales,ICO_Total_Amount,ICO_TotalCount,ICO_HardCap,ICO_Raise_money,Business,Technology,Team,Token,Operation,members,origin,whitepaper,website,cannotareas,Platform,icolink,linkedin";
 $cell=explode(',',$cells);
 $engcells=explode(",","id,name,logo,explodeticker,DataSource,Current_market_value,Current_Single_Price,Current_Circulation,Circulation_unit,Total_Count,Twitter_Fanscount,Facebook_Friends,Telegram_fans,Github_url,GithubCommits,GithubStars,GithubWatches,GithubForks,Github_lastupdatetime,Ico_time,ICO_Price_Usd,ICO_Price_ETH,ICO_Distribution_Ratio,Presales,ICO_Total_Amount,ICO_TotalCount,ICO_HardCap,ICO_Raise_money,Business,Technology,Team,Token,Operation,members,origin,whitepaper,website,cannotareas,Platform,icolink,linkedin");
-exportExcel($name.date("Y-m-d")."csv",$engcells,$data,$engcells);
+exportExcel($name.date("Y-m-d")."csv",$engcells,array(),$engcells);
 function exportExcel($expTitle,$expCellName,$expTableData=array(),$engcell){
     $excel = new PHPExcel();
 
