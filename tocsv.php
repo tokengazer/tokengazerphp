cb1499776d5855bbf3d189efd6b1c2333be70b0f
932563595 <932563595@qq.com>
2018-04-20 01:55:39 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tocsv.php

--- tocsv.php
+++ tocsv.php
@@ -16,13 +16,13 @@ $cells="logo,name,ticker,DataSource,Current_market_value,Current_Single_Price,Cu
 $cell=explode(',',$cells);
 $engcells=explode(",","logo,name,ticker,DataSource,Current_market_value,Current_Single_Price,Current_Circulation,Circulation_unit,Total_Count,Twitter_Fanscount,Facebook_Friends,Telegram_fans,Github_url,GithubCommits,GithubStars,GithubWatches,GithubForks,Github_lastupdatetime,Ico_time,ICO_Price_Usd,ICO_Price_ETH,ICO_Distribution_Ratio,Presales,ICO_Total_Amount,ICO_TotalCount,ICO_HardCap,ICO_Raise_money,Business,Technology,Team,Token,Operation,members,origin,whitepaper,website,cannotareas,Platform,icolink,linkedin");
 exportExcel($name.date("Y-m-d")."csv",$engcells,$data);
-function exportExcel($expTitle,$expCellName,$expTableData){
+function exportExcel($expTitle,$expCellName,$expTableData,$engcell){
     $xlsTitle = iconv('utf-8', 'gb2312', $expTitle);
     $fileName = $_SESSION['loginAccount'].date('_YmdHis');
     $cellNum = count($expCellName);
     $dataNum = count($expTableData);
     $objPHPExcel = new PHPExcel();
-    $cellName=$expCellName;// = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ');
+    $cellName=$engcell;// = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ');
 
     $objPHPExcel->getActiveSheet(0)->mergeCells('A1:'.$cellName[$cellNum-1].'1');
     $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A1', $expTitle.'  Export time:'.date('Y-m-d H:i:s'));
