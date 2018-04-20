d9bf6959847f07984245a835371937fecf1deef8
932563595 <932563595@qq.com>
2018-04-20 09:28:53 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tocsv.php

--- tocsv.php
+++ tocsv.php
@@ -79,7 +79,7 @@ function exportExcel($expTitle,$expCellName,$expTableData,$engcell){
     
         //echo $i,",";continue;
         //设置单元格高度，暂时没有找到统一设置高度方法
-       /* $objActSheet->getRowDimension($i)->setRowHeight('80px');
+        $objActSheet->getRowDimension($i)->setRowHeight('80px');
         header('Content-Type: application/vnd.ms-excel');
     //下载的excel文件名称，为Excel5，后缀为xls，不过影响似乎不大
     $savefile=$expTitle;
@@ -87,7 +87,7 @@ function exportExcel($expTitle,$expCellName,$expTableData,$engcell){
     header('Cache-Control: max-age=0');
     // 用户下载excel
     $objWriter = PHPExcel_IOFactory::createWriter($excel, 'CSV');
-    $objWriter->save('php://output');*/
+    $objWriter->save('php://output');
         }
     
     // 保存excel在服务器上
