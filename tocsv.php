e423a793a1b322a946183c80331c9637baa067af
932563595 <932563595@qq.com>
2018-04-20 08:58:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/tocsv.php

--- tocsv.php
+++ tocsv.php
@@ -49,7 +49,7 @@ function exportExcel($expTitle,$expCellName,$expTableData,$engcell){
     //单独设置D列宽度为15
     $objActSheet->getColumnDimension('D')->setWidth(15);
     //这里$i初始值设置为2，$j初始值设置为0，自己体会原因
-    $pages=floor(count($expTableData)/1000);
+    $pages=floor(count($expTableData)/3000);
     //echo $pages;die;
     for($k=0;$k<$pages;$k++){
         if($k==$pages-1){
@@ -58,7 +58,7 @@ function exportExcel($expTitle,$expCellName,$expTableData,$engcell){
         $limit=($k+1)*1000+1;
         } 
          ob_flush();
-    for ($i = 2+($k*1000);$i <= $limit+2;$i++) {
+    for ($i = 2+($k*3000);$i <= $limit+2;$i++) {
        $j = 0;
         foreach ($expTableData[$i - 2] as $key=>$value) {
             $objActSheet->setCellValue("$letter[$j]$i",$expTableData[$i - 2][$key]);
