3320d467aae189b2ebd55627977644ad5ccc6022
932563595 <932563595@qq.com>
2018-04-23 03:03:21 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/update500githuburl.php

--- update500githuburl.php
+++ update500githuburl.php
@@ -23,14 +23,15 @@ foreach($url1 as $k=>$v){
     $contents1=file_get_contents_https("https://coinmarketcap.com/currencies/".strtolower($url1[$k]['name']).'/');
     $arr[$i]['name']=$url1[$k]['id'];//echo "https://coinmarketcap.com/currencies/".$url1[$k]['id'];
     //print_r($contents1);die;
-    $arr[$i]['githuburl']=$githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','" target="');
+    $arr[$i]['githuburl']=$website=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','" target="');
+    $arr[$i]['website']=explode("\"",explode("<li><span class=\"glyphicon glyphicon-link text-gray\" title=\"Website\"></span> <a href=\"",$contents)[1])[0];
     $kv->delete('products:'.$i);
     $kv->add('products:'.$i, json_encode($arr[$i],true));
     $kv->get('products:'.$i);
     $kv->delete('coinmarketproducts:'.$i);
     $kv->add('coinmarketproducts:'.$i, json_encode($arr[$i],true));
     $kv->get('coinmarketproducts:'.$i);
-    echo $sql="update ico_Analysis set Github_url='".$arr[$i]['githuburl']."' where id=".$url1[$k]['id']."";
+    echo $sql="update ico_Analysis set website='$website',Github_url='".$arr[$i]['githuburl']."' where id=".$url1[$k]['id']."";
         MySQLRunSQL($sql);
     $i++;
 }
