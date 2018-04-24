2e708b235b0859fc146b02720569aef1a2ffeace
932563595 <932563595@qq.com>
2018-04-24 09:36:04 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getgithubdata1.php

--- getgithubdata1.php
+++ getgithubdata1.php
@@ -30,7 +30,7 @@ for($i=0;$i<10;$i++){
             $ch = curl_init();
       		curl_setopt($conn[$k], CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)");   
       		curl_setopt($conn[$k], CURLOPT_HEADER ,0);   
-        	curl_setopt($conn[$k], CURLOPT_RETURNTRANSFER, 1);
+        	curl_setopt($conn[$k], CURLOPT_RETURNTRANSFER, 0);
       		curl_setopt($conn[$k], CURLOPT_CONNECTTIMEOUT,60);   
       		curl_multi_add_handle ($mh,$conn[$k]); 
             $connlist[$k]=$ch;
