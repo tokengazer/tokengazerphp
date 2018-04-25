1ff2fb05c7bbb62e8fc7ad710fd99ecc19c2f221
932563595 <932563595@qq.com>
2018-04-25 03:30:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/getgithubcommits.php

--- getgithubcommits.php
+++ getgithubcommits.php
@@ -54,14 +54,14 @@ function curls($url,$token){
   }
 function  gettotalcommits($user,$pro,$token){
 
-    echo $data_string='{"query":"{\n  repository(owner: \"bitcoin\", name: \"bips\") {\n    name\n    refs(first: 100, refPrefix: \"refs/heads/\") {\n      edges {\n        node {\n          name\n          target {\n            ... on Commit {\n              id\n              history(first: 0) {\n                totalCount\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}","variables":{},"operationName":null}';
+    $data_string='{"query":"{\n  repository(owner: \"'.$user.'\", name: \"'.$pro.'\") {\n    name\n    refs(first: 100, refPrefix: \"refs/heads/\") {\n      edges {\n        node {\n          name\n          target {\n            ... on Commit {\n              id\n              history(first: 0) {\n                totalCount\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}","variables":{},"operationName":null}';
     $headers = array(
         'Authorization:token  '.$token.'',
         'Accept:application/vnd.github.giant-sentry-fist-preview+json',
         'User-Agent: Awesome-Octocat-App',
     );
     $curl = curl_init();
-    echo $url="https://api.github.com/graphql?anon=1000";
+    $url="https://api.github.com/graphql?anon=1000";
     //设置抓取的url
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_POST, 1);
