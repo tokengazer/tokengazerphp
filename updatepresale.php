7949dfacf26b76df6649ecc8071c2ebc13225e06
932563595 <932563595@qq.com>
2018-04-27 10:04:57 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1/updatepresale.php

--- updatepresale.php
+++ updatepresale.php
@@ -10,13 +10,13 @@ $sql="select * from ico_Analysis where Current_market_value<>'';";
 $list=MySQLGetData($sql);
 foreach($list as $kk=>$vv){
     //$url="https://www.feixiaohao.com/currencies/".$list[$kk]['name']."/";
-    $url="https://www.feixiaohao.com/currencies/EOS/";
+    $url="https://www.feixiaohao.com/currencies/icon/";
     $html=file_get_contents_https($url);
     if(strstr($html,"<table class=iCOtable>")) {echo 1;
         $table = explode("<table class=iCOtable>", $html)[1];
                                                 
         $tr = explode("<tr>", $table)[2];print_r($tr);
-        echo $td = explode("<td>", explode("</td>", $tr)[2])[0];
+        echo $td = explode("<td>", explode("<td>", $tr)[2])[0];
 
         echo $sql="update ico_Analysis set presale='$td' where id=".$list[$kk]['id'];
         MySQLRunSQL($sql);
